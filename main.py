a0807f728ce136f6bf7f23bbea8552fbf47b66dc
MartinWho2 <martin.crettol@gmail.com>
2022-04-05 09:55:42 +0200

    Continuing

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import math
 import string
 import time
-
 import pygame
 import json
 import sys
 import random
 
+
+class Team:
+    def __init__(self,members,index):
+        self.team = members
+        self.points = 0
+        self.rounds_won = 0
+        self.index = index
+
 class Button:
-    def __init__(self,rect:pygame.rect.Rect):
+    def __init__(self, rect: pygame.rect.Rect):
         self.rect = rect
-        self.title = pygame.font.Font("Millimetre-Light_web.ttf",round(self.rect.w/4)).render("OK",True,"black")
+        self.title = pygame.font.Font("Millimetre-Light_web.ttf", round(self.rect.w / 4)).render("OK", True, "black")
         self.text_rect = self.title.get_rect(center=self.rect.center)
 
 
@@ -25,7 +32,7 @@ class Input_Rect:
                                                                                                               "black")
         self.rect_input = self.input_text.get_rect(midleft=self.rect.midleft)
 
-    def waiting_for_text(self,window):
+    def waiting_for_text(self, window):
         if self.text == "":
             self.cursor_offset = 3
         else:
@@ -40,15 +47,15 @@ class Input_Rect:
         self.time_bar += 1
         self.time_bar %= 30
 
-    def blit_text(self,window):
+    def blit_text(self, window):
         window.blit(self.input_text, self.rect_input)
 
 
 class Rideaux:
     def __init__(self, title: str, rectangle: pygame.rect.Rect, active=False):
-        self.title = pygame.font.Font("Millimetre-Light_web.ttf", 30).render(title, True, (0,0,0))
+        self.title = pygame.font.Font("Millimetre-Light_web.ttf", 30).render(title, True, (0, 0, 0))
         self.rect: pygame.rect.Rect = rectangle
-        self.surface = pygame.Surface((self.rect.w,self.rect.h), pygame.SRCALPHA)
+        self.surface = pygame.Surface((self.rect.w, self.rect.h), pygame.SRCALPHA)
         title_rect = self.title.get_rect(center=self.surface.get_rect().center)
         self.surface.fill((248, 248, 248))
         self.surface.blit(self.title, title_rect)
@@ -66,7 +73,7 @@ class Rideaux:
 
 
 class Turn:
-    def __init__(self, display: pygame.surface.Surface, title: str):
+    def __init__(self, display: pygame.surface.Surface, title: str,TEAMS=[]):
         self.activated = False
         size = display.get_size()
         self.input_rects = []
@@ -77,50 +84,73 @@ class Turn:
                           (0, 0))
         self.title_text = title
         self.title = pygame.font.Font("Minipax-Medium.ttf", 100).render(title, True, "black")
-        #self.title = pygame.transform.scale(self.title,(round(self.title.get_width()*1.4),self.title.get_height()))
+        # self.title = pygame.transform.scale(self.title,(round(self.title.get_width()*1.4),self.title.get_height()))
         title_rect = self.title.get_rect(center=(window.get_width() / 2, window.get_height() * 0.2))
+        if title[0] == "R":
+            title_rect.centery = round(window.get_height()*0.15)
         self.surface.blit(self.title, title_rect)
         self.window = display
         self.rect_selected_bool = False
         self.rect_selected = 0
-        if title[0] in {"D","T"}:
+        if title[0] in {"D", "T"}:
             self.blit_not_allowed()
+        if title[0] == "R":
+            self.blit_results(TEAMS)
+
+    def blit_results(self,two_participants:list[list]):
+        for i_match in range(4):
+            for i_team in range(2):
+                i = i_match*2 + i_team
+                rect = pygame.rect.Rect(round(self.window.get_width() /5),round(i*self.window.get_height()*0.08+self.window.get_height()*0.23),
+                                    round(self.window.get_width()/3),round(self.window.get_height()*0.07))
+                draw_rounded_rect(self.surface,rect,(200,200,200),(248,252,255))
+                team = two_participants[i_match][i_team]
+                text = f"{team[0]} & {team[1]}"
+                name = font.render(text, True, "black")
+                name_rect = name.get_rect(center=rect.center)
+                self.surface.blit(name, name_rect)
+
     def activate(self):
         if not self.activated:
             self.activated = True
+
     def blit_not_allowed(self):
-        text = pygame.font.Font("Millimetre-Light_web.ttf",90).render("PAS ENCORE DÉFINI",True,"black")
-        text_rect = text.get_rect(center=(round(self.window.get_width()/2),round(self.window.get_height()/2)))
-        self.surface.blit(text,text_rect)
+        text = pygame.font.Font("Millimetre-Light_web.ttf", 90).render("PAS ENCORE DÉFINI", True, "black")
+        text_rect = text.get_rect(center=(round(self.window.get_width() / 2), round(self.window.get_height() / 2)))
+        self.surface.blit(text, text_rect)
 
     def blit_participants(self, two_persons: list):
-        center_rect = pygame.rect.Rect(round(self.window.get_width()/4),round(self.window.get_height()/3),round(self.window.get_width()/2),round(self.window.get_height()/2))
-        pygame.draw.rect(self.surface,(248,252,255),center_rect)
+        center_rect = pygame.rect.Rect(round(self.window.get_width() / 4), round(self.window.get_height() / 3),
+                                       round(self.window.get_width() / 2), round(self.window.get_height() / 2))
+        pygame.draw.rect(self.surface, (248, 252, 255), center_rect)
         y_poses = [0.3, 0.39, 0.52, 0.61]
         size = self.window.get_size()
         if self.title_text[0] != "R":
             for column in range(2):
                 for row in range(4):
-                    a = Input_Rect(pygame.rect.Rect(round(size[0]/48+size[0]*6/15+column*size[0]/12),
-                                                    round(size[1]*y_poses[row]),round(size[0]/24),round(size[1]*0.08)))
+                    a = Input_Rect(pygame.rect.Rect(round(size[0] / 48 + size[0] * 6 / 15 + column * size[0] / 12),
+                                                    round(size[1] * y_poses[row]), round(size[0] / 24),
+                                                    round(size[1] * 0.08)))
                     self.input_rects.append(a)
-            button_size = (round(size[0]/15),round(size[1]/15))
-            self.ok = Button(pygame.rect.Rect(round(size[0]/2-button_size[0]/2),round(size[1]*0.8),button_size[0],button_size[1]))
-            pygame.draw.rect(self.surface,"black",self.ok.rect,width = 2)
-            self.surface.blit(self.ok.title,self.ok.text_rect)
+            button_size = (round(size[0] / 15), round(size[1] / 15))
+            self.ok = Button(
+                pygame.rect.Rect(round(size[0] / 2 - button_size[0] / 2), round(size[1] * 0.8), button_size[0],
+                                 button_size[1]))
+            pygame.draw.rect(self.surface, "black", self.ok.rect, width=2)
+            self.surface.blit(self.ok.title, self.ok.text_rect)
         for match in range(len(two_persons)):
             # pygame.draw.rect(first_turn, "orange", pygame.rect.Rect(50,round(match * window.get_height()/13*3 + window.get_height()/18),round(window.get_width()*5/6),round(window.get_height()/5)))
             for team in range(2):
                 x_width = round(self.window.get_width() / 3)
                 y_height = round(self.window.get_height() * 0.08)
                 y_between = round(self.window.get_height() * 0.01)
-                x_pos = round(self.window.get_width() * 1 / 15) + math.floor(match / 2) * round(
+                x_pos = round(self.window.get_width() / 15) + math.floor(match / 2) * round(
                     self.window.get_width() * 0.5)
                 y_offset = round(self.window.get_height() * 0.30)
                 y_pos = y_offset + (match % 2) * round(self.window.get_height() * 0.22) + team * (y_height + y_between)
                 rect = pygame.rect.Rect(x_pos, y_pos, x_width, y_height)
-                #pygame.draw.rect(self.surface, (200, 200, 200), rect)
-                draw_rounded_rect(self.surface,rect,(200,200,200),(248,252,255))
+                # pygame.draw.rect(self.surface, (200, 200, 200), rect)
+                draw_rounded_rect(self.surface, rect, (200, 200, 200), (248, 252, 255))
                 members = two_persons[match][team]
                 text = f"{members[0]} & {members[1]}"
                 name = font.render(text, True, "black")
@@ -128,33 +158,33 @@ class Turn:
                 self.surface.blit(name, name_rect)
 
 
-def draw_rounded_rect(surface:pygame.surface,rect,rect_color,bg_color):
+def draw_rounded_rect(surface: pygame.surface, rect, rect_color, bg_color):
     rounding = 35
-    pygame.draw.rect(surface,rect_color,rect)
-    quarter_circle = pygame.surface.Surface((rounding,rounding))
+    pygame.draw.rect(surface, rect_color, rect)
+    quarter_circle = pygame.surface.Surface((rounding, rounding))
     quarter_circle.fill(bg_color)
-    pygame.draw.circle(quarter_circle,rect_color,(rounding,rounding),rounding)
-    surface.blit(quarter_circle,(rect.x,rect.y))
-    quarter_circle = pygame.transform.rotate(quarter_circle,90)
-    surface.blit(quarter_circle,(rect.x,rect.bottom-rounding))
+    pygame.draw.circle(quarter_circle, rect_color, (rounding, rounding), rounding)
+    surface.blit(quarter_circle, (rect.x, rect.y))
     quarter_circle = pygame.transform.rotate(quarter_circle, 90)
-    surface.blit(quarter_circle, (rect.right-rounding, rect.bottom - rounding))
+    surface.blit(quarter_circle, (rect.x, rect.bottom - rounding))
     quarter_circle = pygame.transform.rotate(quarter_circle, 90)
-    surface.blit(quarter_circle, (rect.right-rounding, rect.y))
+    surface.blit(quarter_circle, (rect.right - rounding, rect.bottom - rounding))
+    quarter_circle = pygame.transform.rotate(quarter_circle, 90)
+    surface.blit(quarter_circle, (rect.right - rounding, rect.y))
 
 
-def create_new_matches(turn_number:int, textss: list[str]):
+def create_new_matches(turn_number: int, textss: list[str]):
     winners = []
     losers = []
     for match in range(4):
-        team1 = int(textss[match*2])
-        team2 = int(textss[match*2+1])
+        team1 = int(textss[match * 2])
+        team2 = int(textss[match * 2 + 1])
         if team1 > team2:
-            winners.append(match*2)
-            losers.append(match*2+1)
+            winners.append(match * 2)
+            losers.append(match * 2 + 1)
         else:
-            losers.append(match*2)
-            winners.append(match*2+1)
+            losers.append(match * 2)
+            winners.append(match * 2 + 1)
     if turn_number == 0:
         matchs = []
         n_teams = 4
@@ -179,9 +209,9 @@ def create_new_matches(turn_number:int, textss: list[str]):
         print(matchs)
         return matchs
     if turn_number == 1:
-        matchs = [[winners[0],winners[1]]]
+        matchs = [[winners[0], winners[1]]]
         n_teams = 4
-        middle_players = [winners[2],winners[3],losers[0],losers[1]]
+        middle_players = [winners[2], winners[3], losers[0], losers[1]]
         for match in range(2):
             nth_match = []
             for team in range(2):
@@ -190,7 +220,7 @@ def create_new_matches(turn_number:int, textss: list[str]):
                 del middle_players[selected]
                 n_teams -= 1
             matchs.append(nth_match)
-        matchs.append([losers[1],losers[2]])
+        matchs.append([losers[2], losers[3]])
         return matchs
 
 
@@ -203,14 +233,6 @@ with open("teams.json", "r", encoding="UTF-8") as file:
 window = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
 font = pygame.font.Font("Millimetre-Light_web.ttf", 25)
-screens_titles = ["Premier Tour", "Deuxième Tour", "Troisième Tour", "Résultats"]
-screens = []
-rideaux = []
-for i in range(4):
-    screens.append(Turn(window, screens_titles[i]))
-    rideaux.append(Rideaux(screens_titles[i], pygame.rect.Rect(round(window.get_width() / 4 * i), 0,
-    round(window.get_width() / 4), round(window.get_height() * 0.08)),active=False if i else True))
-active_screen = 0
 
 # There should be 8 teams
 n_teams = 8
@@ -225,14 +247,28 @@ for match in range(4):
         n_teams -= 1
     matches.append(nth_match)
 
+screens_titles = ["Premier Tour", "Deuxième Tour", "Troisième Tour", "Résultats"]
+screens = []
+rideaux = []
+for i in range(4):
+    if i == 3:
+        screens.append(Turn(window, screens_titles[i],TEAMS=matches))
+    else:
+        screens.append(Turn(window, screens_titles[i]))
+    rideaux.append(Rideaux(screens_titles[i], pygame.rect.Rect(round(window.get_width() / 4 * i), 0,
+                                                               round(window.get_width() / 4),
+                                                               round(window.get_height() * 0.08)),
+                           active=False if i else True))
+active_screen = 0
+
 screens[0].blit_participants(matches)
 while True:
     window.fill((0, 0, 0))
     window.blit(screens[active_screen].surface, (0, 0))
     for rideau in rideaux:
-        window.blit(rideau.surface,(rideau.rect.x,rideau.rect.y))
+        window.blit(rideau.surface, (rideau.rect.x, rideau.rect.y))
     for input_text in screens[active_screen].input_rects:
-        pygame.draw.rect(window,"black",input_text.rect,width=1)
+        pygame.draw.rect(window, "black", input_text.rect, width=1)
         input_text.blit_text(window)
     if screens[active_screen].rect_selected_bool:
         screens[active_screen].input_rects[screens[active_screen].rect_selected].waiting_for_text(window)
@@ -243,7 +279,8 @@ while True:
             if screens[active_screen].rect_selected_bool:
                 text = screens[active_screen].input_rects[screens[active_screen].rect_selected].text
                 if e.key == pygame.K_BACKSPACE:
-                    screens[active_screen].input_rects[screens[active_screen].rect_selected].text = screens[active_screen].input_rects[screens[active_screen].rect_selected].text[:-1]
+                    screens[active_screen].input_rects[screens[active_screen].rect_selected].text = \
+                    screens[active_screen].input_rects[screens[active_screen].rect_selected].text[:-1]
                 elif e.key == pygame.K_RETURN:
                     screens[active_screen].rect_selected_bool = False
                 elif len(text) < 4 and e.unicode in string.printable[0:10]:
@@ -269,8 +306,8 @@ while True:
                         active_screen = i
                         break
                 else:
-                    if hasattr(screens[active_screen],"ok"):
-                        if screens[active_screen].ok.rect.collidepoint(e.pos[0],e.pos[1]):
+                    if hasattr(screens[active_screen], "ok"):
+                        if screens[active_screen].ok.rect.collidepoint(e.pos[0], e.pos[1]):
                             texts = []
                             for input_text in screens[active_screen].input_rects:
                                 texts.append(input_text.text)
@@ -290,13 +327,15 @@ while True:
                                     screens[active_screen + 1].blit_participants(new_matches)
                                 elif active_screen == 1:
                                     print(new_matches)
-                                    new_new_matches = create_new_matches(active_screen,texts)
+                                    new_new_matches = create_new_matches(active_screen, texts)
+                                    print(new_new_matches)
                                     for i in range(len(new_new_matches)):
                                         for j in range(2):
                                             team_index = new_new_matches[i][j]
-                                            i_j_index = [math.floor(team_index/2),team_index%2]
+                                            i_j_index = [math.floor(team_index / 2), team_index % 2]
                                             new_new_matches[i][j] = new_matches[i_j_index[0]][i_j_index[1]]
-                                    screens[active_screen + 1].blit_participants(new_matches)
+                                    print(new_new_matches)
+                                    screens[active_screen + 1].blit_participants(new_new_matches)
 
         if e.type == pygame.QUIT:
             pygame.quit()
